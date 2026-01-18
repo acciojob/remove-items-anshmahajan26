0dcb4966b4211a0bbb4033db9580bc3753b4d22f
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-18 23:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// get select element
+let select = document.getElementById("colorSelect");
+
+// get the button
+let btn = document.querySelector('input[type="button"]');
+
+// add click event
+btn.addEventListener("click", function () {
+  // get selected index
+  let index = select.selectedIndex;
+
+  // remove selected option
+  if (index !== -1) {
+    select.remove(index);
+  }
+});
